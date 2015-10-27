a2843731d00fa2e5b9f5b465e734da68901dda84
Brian Blosser <bpb54321@gmail.com>
2015-10-27 14:17:35 -0500

    Changed index.php to show a 'page not found' error, with a link to the Winding Rivers website.

---
+++ wp-content/themes/BootstrapWP/index.php
@@ -0,0 +1,32 @@
+
+<!--Used by Wordpress as a back-up for all pages.  front-page.php will always take precedence over this file-->
+    
+    <?php get_header(); ?>
+
+
+    <div class="jumbotron" >
+      <div class="container" >
+        <h2>This page does not exist on our site.  To return to our homepage, please visit the following link: <a href="http://www.windingrivers.org">http://www.windingrivers.org</a> </h2>
+      </div>
+    </div>
+
+    <div class="supporting-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h2 style="text-align: center;">Vision</h2>
+              <p>
+                We are a cooperative residential community embodying shared values of social justice, affordability, diversity, and ecological living.
+              </p>
+          </div>
+          <div class="col-md-6">
+            <h2 style="text-align: center;">Mission</h2>
+            <p>
+              Our mission is to foster lives of connection and well-being for 12-20 diverse households in the St. Louis metropolitan area through living in an ecologically sustainable way, sharing our time and resources with each other, and building strong, healthy relationships within our group and with the surrounding community.
+            </p>
+          </div>
+        </div> 
+      </div>
+    </div>
+
+    <?php get_footer(); ?>
